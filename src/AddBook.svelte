d3336d7b501ee3746bf23a8c9ccc3b1eee2b2c5b
Yannick_van_Leeuwen <yannick.vanleeuwen@stacker.nl>
2023-11-28 15:19:44 +0100

    setup project

---
+++ src/AddBook.svelte
@@ -0,0 +1,101 @@
+<script>
+    import { onMount } from 'svelte';
+    import 'bootstrap/dist/css/bootstrap.min.css';
+  
+    let books = [];
+    let newBook = {
+      title: '',
+      description: '',
+      author: '',
+      numberOfPages: 0,
+      // Add more properties as needed
+    };
+  
+    onMount(async () => {
+      await fetchBooks();
+    });
+  
+    const fetchBooks = async () => {
+      const response = await fetch('http://your-csharp-api-url/api/books');
+      if (response.ok) {
+        books = await response.json();
+      }
+    };
+  
+    const addBook = async () => {
+      const response = await fetch('http://your-csharp-api-url/api/books', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newBook),
+      });
+  
+      if (response.ok) {
+        // Clear the form and fetch the updated list of books
+        newBook = {
+          title: '',
+          description: '',
+          author: '',
+          numberOfPages: 0,
+        };
+        await fetchBooks();
+      } else {
+        console.error('Failed to add the book');
+      }
+    };
+  </script>
+  
+  <main>
+    <h1>Book List</h1>
+    <table class="table">
+      <!-- Table header as before -->
+  
+      <tbody>
+        {#each books as book (book.id)}
+          <tr>
+            <!-- Table cells as before -->
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  
+    <h2>Add New Book</h2>
+    <form on:submit|preventDefault={addBook}>
+      <label>
+        Title:
+        <input bind:value={newBook.title} required />
+      </label>
+      <br />
+  
+      <label>
+        Description:
+        <input bind:value={newBook.description} required />
+      </label>
+      <br />
+  
+      <label>
+        Author:
+        <input bind:value={newBook.author} required />
+      </label>
+      <br />
+  
+      <label>
+        Number of Pages:
+        <input bind:value={newBook.numberOfPages} type="number" required />
+      </label>
+      <br />
+  
+      <!-- Add more input fields for other properties -->
+  
+      <button type="submit">Add Book</button>
+    </form>
+  </main>
+  
+  <style>
+    main {
+      text-align: center;
+      margin: 2em;
+    }
+  </style>
+  
---
+++ src/BookList.svelte
@@ -0,0 +1,50 @@
+<script>
+    import { onMount } from "svelte";
+    import 'bootstrap/dist/css/bootstrap.min.css';
+  
+    let books = [];
+  
+    // Fetch data from the C# backend when the component mounts
+    onMount(async () => {
+      const response = await fetch("https://localhost:7125/api/books");
+      if (response.ok) {
+        books = await response.json();
+      }
+    });
+  </script>
+  
+  <main>
+    <h1>Book List</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Title</th>
+          <th scope="col">Description</th>
+          <th scope="col">Author</th>
+          <th scope="col">Number of Pages</th>
+          <!-- Add more columns for other properties -->
+        </tr>
+      </thead>
+      <tbody>
+        {#each books as book (book.id)}
+          <tr>
+            <th scope="row">{book.id}</th>
+            <td>{book.title}</td>
+            <td>{book.description}</td>
+            <td>{book.author}</td>
+            <td>{book.numberOfPages}</td>
+            <!-- Add more cells for other properties -->
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </main>
+  
+  <style>
+    main {
+      text-align: center;
+      margin: 2em;
+    }
+  </style>
+  
